2dce1f9b81e562729b659a4d4d991bddff3dc53c
SyAtlantis <1102960695@qq.com>
2019-03-29 14:09:34 +0800

    添加返回index

--- src/api/lottery.js
+++ src/api/lottery.js
@@ -6,14 +6,9 @@ const Ajv = require("ajv");
 let ajv = new Ajv();
 
 const schema = require("../schema/ajv-lottery.js");
-
 const Permutations = require("../utils/permutations");
 
-// const a = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33"];
-// const b = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16"];
-
 const getLottery = async ctx => {
-    // let query = ctx.query;
     let query = ctx.body;
 
     let valid = ajv.validate(schema.lottery, query);
@@ -30,18 +25,16 @@ const getLottery = async ctx => {
     let hash = query.hash;
 
     try {
-        // console.time("permute");
         let lottery = null;
         let number = new BigNumber(hash, 16);
         let size = Permutations.calcPermute(data);
         let index = number.mod(size).toNumber();
         lottery = Permutations.getMixingByIndex(data, index);
-        // console.log(size, index, lottery);
-        // console.timeEnd("permute");
 
         if (lottery) {
             ctx.body = {
                 success: true,
+                index,
                 lottery
             };
             return;
